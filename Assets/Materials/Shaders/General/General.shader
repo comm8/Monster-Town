333bc09c532762be8d1f86b5b0607918a7df3337
Evelyn Comeau <jiraa8@protonmail.com>
2023-10-29 13:28:46 -0700

    More stuff for shaders! ╰(*°▽°*)╯

--- Assets/Materials/Shaders/General/General.shader
+++ Assets/Materials/Shaders/General/General.shader
@@ -1,31 +1,30 @@
-Shader "Unlit/General"
-{
-
-	SubShader{
-		Tags{"RenderPipeline" = "UniversalPipeline"}
-
-		Pass {
-			Name "ForwardLit"
-			Tags{"LightMode" = "UniversalForward"}
+Shader "Shaders/General" {
+    // Subshaders allow for different behaviour and options for different pipelines and platforms
+Properties{
+	[Header(Surface options)]
+	[MainColor] _ColorTint("Tint", Color) = (1,1,1,1)
+	}
 
 
-			HLSLPROGRAM
+    SubShader{
+        // These tags are shared by all passes in this sub shader
+        Tags{"RenderPipeline" = "UniversalPipeline"}
 
-			#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary.Lighting.hlsl"
+        // Shaders can have several passes which are used to render different data about the material
+        // Each pass has it's own vertex and fragment function and shader variant keywords
+        Pass {
+            Name "ForwardLit" // For debugging
+            Tags{"LightMode" = "UniversalForward"} // Pass specific tags. 
+            // "UniversalForward" tells Unity this is the main lighting pass of this shader
 
-			struct Attributes {
-				float3 position : POSITION;
-				};
+            HLSLPROGRAM // Begin HLSL code
+            // Register our programmable stage functions
+            #pragma vertex Vertex
+            #pragma fragment Fragment
 
-				void Vertex(Attributes input)
-
-			ENDHLSL
-
-			}
-
-
-
-		}
-		 
-
+            // Include our code file
+            #include "GeneralForwardLitPass.hlsl"
+            ENDHLSL
+        }
+    }
 }
--- Assets/Materials/Shaders/General/GeneralForwardLitPass.hlsl
+++ Assets/Materials/Shaders/General/GeneralForwardLitPass.hlsl
@@ -1,29 +1,41 @@
-			#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary.Lighting.hlsl"
+// Pull in URP library functions and our own common functions
+#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
 
-            struct interpolators {
-            float4 positionCS : SV_POSITION;
-            }
-            
-			struct Attributes
-                {
-				float3 position : POSITION; //Pos in OBJ space
-				};
-
-            Interpolators Vertex(Attributes input) {
-            VertexPositionInputs posnInputs = GetVertexPositionInputs(input.PositionOS)
+float4 _ColorTint;
 
 
-            output.positionCS = posnInputs.positionCS;
+// This attributes struct receives data about the mesh we're currently rendering
+struct Attributes {
+	float3 positionOS : POSITION; // Position in object space
+};
 
-            return output;
+// This struct is output by the vertex function and input to the fragment function.
+// Note that fields will be transformed by the intermediary rasterization stage
+struct Interpolators {
+	// This value should contain the position in clip space (which is similar to a position on screen)
+	// when output from the vertex function. It will be transformed into pixel position of the current
+	// fragment on the screen when read from the fragment function
+	float4 positionCS : SV_POSITION;
+};
 
-            }
+// The vertex function. This runs for each vertex on the mesh.
+// It must output the position on the screen each vertex should appear at,
+// as well as any data the fragment function will need
+Interpolators Vertex(Attributes input) {
+	Interpolators output;
 
+	// These helper functions, found in URP/ShaderLib/ShaderVariablesFunctions.hlsl
+	// transform object space values into world and clip space
+	VertexPositionInputs posnInputs = GetVertexPositionInputs(input.positionOS);
 
+	// Pass position and orientation data to the fragment function
+	output.positionCS = posnInputs.positionCS;
 
-				void Vertex(Attributes input)
-                {
-                    VertexPositionInputs posnInputs = GetVertexPositionInputs(input.position);
+	return output;
+}
 
-
-                }
+// The fragment function. This runs once per fragment, which you can think of as a pixel on the screen
+// It must output the final color of this pixel
+float4 Fragment(Interpolators input) : SV_TARGET {
+	return float4(_ColorTint);
+}
