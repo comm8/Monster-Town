8cf156026d2064ea3e0227f510bfb7494fd3bd72
Evelyn Comeau <jiraa8@protonmail.com>
2023-12-12 19:37:51 -0800

    Started work on hooking into ECS with monoBehavior

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,14 +1,18 @@
 using UnityEngine;
 using CameraUtilities;
-
+using Unity.Entities;
 public class UIManager : MonoBehaviour
 {
    [SerializeField] Transform cameraTransform;
     void Update()
     {
-       Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         Physics.Raycast(ray, out hit);
         Debug.Log("CURRENT UI" + CameraUtil.PositionToTile(hit.point));
+
+        var EM = World.DefaultGameObjectInjectionWorld.EntityManager;
+        //Somehow get the the corresponding Tile from ECS
+
     }
 }
