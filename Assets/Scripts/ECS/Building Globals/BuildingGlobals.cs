a116b8052a4f590aff8d10af7abc1fc55c216db4
Evelyn Comeau <jiraa8@protonmail.com>
2023-12-19 20:35:48 -0800

    Big changes coming :+1:

--- Assets/Scripts/ECS/Building Globals/BuildingGlobals.cs
+++ Assets/Scripts/ECS/Building Globals/BuildingGlobals.cs
@@ -1,11 +1,11 @@
 using Unity.Entities;
 using Unity.Burst;
-
 namespace BuildingTools
 {
   [BurstCompile]
   public struct BuildingGlobals : IComponentData
   {
+        public bool finishedEntityCreation;
 
     public int gridSize;
     public Entity buildingPrefab;
@@ -26,11 +26,24 @@ namespace BuildingTools
 
   }
 
+    public readonly partial struct BuildingGlobalsAspect : IAspect
+    {
+        public readonly DynamicBuffer<BuildingAspectListBuffer> buildingAspectListBuffers;
+        public readonly RefRW<BuildingGlobals> globals;
+
+        public int gridSize
+        {
+            get => globals.ValueRO.gridSize;
+            set => globals.ValueRW.gridSize = value;
+        }
+
+    }
+
     // This describes the number of buffer elements that should be reserved
     // in chunk data for each instance of a buffer. In this case, 8 integers
     // will be reserved (32 bytes) along with the size of the buffer header
     // (currently 16 bytes on 64-bit targets)
-    [InternalBufferCapacity(8)]
+    [InternalBufferCapacity(3)]
     public struct BuildingAspectListBuffer : IBufferElementData
     {
         // These implicit conversions are optional, but can help reduce typing.
@@ -41,3 +54,10 @@ namespace BuildingTools
         public BuildingAspect Value;
     }
 }
+
+
+
+
+
+
+
