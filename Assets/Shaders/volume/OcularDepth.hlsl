1131673f3e36674bccfe7893e29461270fa0fd18
Evelyn Comeau <jiraa8@protonmail.com>
2023-12-29 14:15:35 -0800

    FINALLY IT SAMPLES CORRECTLY

--- Assets/Shaders/volume/OcularDepth.hlsl
+++ Assets/Shaders/volume/OcularDepth.hlsl
@@ -1,7 +1,7 @@
 #ifndef OCULARDEPTH_HLSL
 #define OCULARDEPTH_HLSL
 
-#include "Packages/com.jimmycushnie.noisynodes/NoiseShader/HLSL/SimplexNoise3D.hlsl" 
+#include "Packages/com.jimmycushnie.noisynodes/NoiseShader/HLSL/ClassicNoise3D.hlsl" 
 
 void CalculateOcularDepth_float( float3 RayOrigin, float3 Ray_Direction, float Steps, float Scene_Depth, float Step_Length, float Atmospheric_Density, out float Optical_Depth)
 {
@@ -15,13 +15,13 @@ if(Scene_Depth < (Steps * Step_Length ))
 
 for(int i = 0; i < Steps; i++)
 {
-    Optical_Depth += snoise((RayOrigin + (Ray_Direction * i))) * Step_Length;
+    Optical_Depth += cnoise((RayOrigin + (Ray_Direction * i))) * Step_Length;
 }
 
 Optical_Depth = (Optical_Depth * Steps)/Step_Length;
  
+ Optical_Depth = (cnoise((RayOrigin + Ray_Direction) * 0.1) + 1.15)/2.3;
 }
-
 #endif
 
 
