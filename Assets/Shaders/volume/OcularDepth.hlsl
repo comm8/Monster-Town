663f8e164f078f5b9405cfb029deb66f7badf14e
Evelyn Comeau <jiraa8@protonmail.com>
2023-12-29 17:32:09 -0800

    no

--- Assets/Shaders/volume/OcularDepth.hlsl
+++ Assets/Shaders/volume/OcularDepth.hlsl
@@ -3,7 +3,7 @@
 
 #include "Packages/com.jimmycushnie.noisynodes/NoiseShader/HLSL/ClassicNoise3D.hlsl" 
 
-void CalculateOcularDepth_float( float3 RayOrigin, float3 Ray_Direction, float Steps, float Scene_Depth, float Step_Length, float Atmospheric_Density, out float Optical_Depth)
+void CalculateOcularDepth_float( float3 RayOrigin, float3 Ray_Direction, float Steps, float Scene_Depth, float Step_Length, float Atmospheric_Density, float Noise_Scale, out float Optical_Depth)
 {
 
 Optical_Depth = 0;
@@ -15,7 +15,7 @@ if(Scene_Depth < (Steps * Step_Length ))
 
 for(int i = 0; i < Steps; i++)
 {
- Optical_Depth += (cnoise((RayOrigin + (Ray_Direction * i * Step_Length )) * 0.01) + 1.15)/2.3;
+ Optical_Depth += (cnoise((RayOrigin + (Ray_Direction * i * Step_Length )) * Noise_Scale) + 1.15)/2.3;
 }
 
 Optical_Depth = Optical_Depth/Step_Length;
