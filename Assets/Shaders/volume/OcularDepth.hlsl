61a912d40b5178edf8e8c02e560250ed7022b4d4
Evelyn Comeau <jiraa8@protonmail.com>
2023-12-29 18:04:20 -0800

    WHY WONT IT WORK

--- Assets/Shaders/volume/OcularDepth.hlsl
+++ Assets/Shaders/volume/OcularDepth.hlsl
@@ -3,22 +3,29 @@
 
 #include "Packages/com.jimmycushnie.noisynodes/NoiseShader/HLSL/ClassicNoise3D.hlsl" 
 
-void CalculateOcularDepth_float( float3 RayOrigin, float3 Ray_Direction, float Steps, float Scene_Depth, float Step_Length, float Atmospheric_Density, float Noise_Scale, out float Optical_Depth)
+void CalculateOcularDepth_float( float3 RayOrigin, float3 Ray_Direction, float Steps, float Scene_Depth, float Sample_radius, float Atmospheric_Density, float Noise_Scale, out float Optical_Depth)
 {
 
 Optical_Depth = 0;
 
-if(Scene_Depth < (Steps * Step_Length ))
+float Step_Length = 0;
+
+if(Scene_Depth < Sample_radius)
 {
-    Steps = Scene_Depth/Step_Length;
+    Step_Length = Scene_Depth/Steps;
+}
+else
+{
+    Step_Length = Sample_radius/Steps;
 }
 
 for(int i = 0; i < Steps; i++)
 {
- Optical_Depth += (cnoise((RayOrigin + (Ray_Direction * i * Step_Length )) * Noise_Scale) + 1.15)/2.3;
+ Optical_Depth += (cnoise((RayOrigin + (Ray_Direction * (i * Step_Length))) * Noise_Scale) + 1.15)/2.3;
 }
 
-Optical_Depth = Optical_Depth/Step_Length;
+Optical_Depth = Optical_Depth * Step_Length;
+ Optical_Depth = (cnoise((RayOrigin + (Ray_Direction * Scene_Depth)) * Noise_Scale) + 1.15)/2.3;
 
 }
 #endif
