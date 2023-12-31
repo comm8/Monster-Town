6f35fab29488a7577c738c388c946bc41b97432a
Evelyn Comeau <jiraa8@protonmail.com>
2023-12-30 20:21:11 -0800

    FOG :D

--- Assets/Shaders/volume/OcularDepth.hlsl
+++ Assets/Shaders/volume/OcularDepth.hlsl
@@ -3,7 +3,7 @@
 
 #include "Packages/com.jimmycushnie.noisynodes/NoiseShader/HLSL/ClassicNoise3D.hlsl" 
 
-void CalculateOcularDepth_float( float3 RayOrigin, float3 Ray_Direction, float Steps, float Scene_Depth, float Sample_radius, float Atmospheric_Density, float Noise_Scale, out float Optical_Depth)
+void CalculateOcularDepth_float( float3 RayOrigin, float3 Ray_Direction, float Steps, float Sample_radius, float Scene_Depth, float Atmospheric_Density, float Noise_Scale, out float Optical_Depth)
 {
 
 Optical_Depth = 0;
@@ -21,12 +21,16 @@ else
 
 for(int i = 0; i < Steps; i++)
 {
- Optical_Depth += (cnoise((RayOrigin + (Ray_Direction * (i * Step_Length))) * Noise_Scale) + 1.15)/2.3;
+ Optical_Depth += cnoise( (RayOrigin + (Ray_Direction * (i * Step_Length)) )  * Noise_Scale);
 }
 
-Optical_Depth = Optical_Depth * Step_Length;
- Optical_Depth = (cnoise((RayOrigin + (Ray_Direction * Scene_Depth)) * Noise_Scale) + 1.15)/2.3;
+Optical_Depth = (Optical_Depth + 1.15)/2.3; //account for weird stuff going on with cnoise function
 
+Optical_Depth = (Optical_Depth * Step_Length);
+
+
+Optical_Depth = exp(-Optical_Depth * 0.1);
+// transmittance = exp(-(sunRayOpticalDepth + viewRayOpticalDepth) * scatteringCoefficients)
 }
 #endif
 
