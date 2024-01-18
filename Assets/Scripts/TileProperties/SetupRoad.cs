51084ddc59c7683c17b76c6dc1614045efe38738
Evelyn <jiraa8@protonmail.com>
2024-01-18 09:44:43 -0800

    Road

--- Assets/Scripts/TileProperties/SetupRoad.cs
+++ Assets/Scripts/TileProperties/SetupRoad.cs
@@ -1,41 +1,72 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using SerializableDictionary.Scripts;
 
 public class SetupRoad : MonoBehaviour
 {
 
     [SerializeField] float squareSize;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+      [SerializeField] SerializableDictionary<AdjustedBoolTable, Vector2> roadShapeDictionary;
+
+        AdjustedBoolTable adjustedBools;
 
     void checkRoadDirectionDesired(Vector2 UV)
     {
+    RoadBoolTable roadBools = new RoadBoolTable();
         if ((UV.x > 0.5 - squareSize && UV.x < 0.5 + squareSize) && (UV.y > 0.5 - squareSize && UV.y < 0.5 + squareSize))
         {
-            //we are in the center square
+            roadBools.center = true;
             return;
         }
 
         if (1 > UV.x / UV.y)
         {
-            //we are in the bottom left
-            return;
+            roadBools.topleft = true;
+            //we are in the top left
+        }
+        else
+        {
+            roadBools.bottomright = true;
+            //we are in the bottom right
         }
 
         if(-1 > (UV.x - 1) / UV.y)
         {
-            //we are in the top right quad
-            return;
+            roadBools.bottomleft = true;
+            //we are in the bottom left
         }
+        else
+        {
+            roadBools.topright = false;
+            //we are in the top right
+        }
+
+        combo(roadBools, adjustedBools);
+
+        UV = roadShapeDictionary.Get(adjustedBools);
     }
 
-    // Update is called once per frame
-    void Update()
+    void combo(RoadBoolTable road, AdjustedBoolTable adjustedRoad)
     {
-        
+            adjustedRoad.top = (road.topleft && road.topright);
+            adjustedRoad.bottom = (road.bottomleft && road.bottomright);
+            adjustedRoad.right = (road.topright && road.bottomright);
+            adjustedRoad.left = (road.topleft && road.bottomleft);
     }
+
+    public class RoadBoolTable
+    {
+        public bool topleft, topright, bottomleft, bottomright, center;
+    }
+
+    public class AdjustedBoolTable
+    {
+        public bool left, right, bottom, top, center;
+    }
+
+    void updateRoadTex()
+    {
+
+    }
+
 }
