45b6fcd7058fe8c936fc1f8077c46fda1f13ac1f
Evelyn <jiraa8@protonmail.com>
2024-03-15 09:21:59 -0700

    Bunches of stuff

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Attack
+{
+    public abstract void Run(Byte damage);
+
+
+    
+}
